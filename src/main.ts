7c195458d44db57bc76565b5d89dd97d9180e499
razumcevvaa <katerazumceva@gmail.com>
2024-03-07 21:14:12 +0300

    практика 5

--- src/main.ts
+++ src/main.ts
@@ -711,12 +711,93 @@ console.log(getRemaiderOfNum(11, 3))
 // задание 7
 // Написать функцию, которая принимает от 1 до 5 чисел и
 // возвращает их сумму.
-// function sumNumbers(...numbers:[]) {
-//     return numbers.reduce((acc, curr) => acc + curr, 0)
-// }
-// console.log(sumNumbers(1,2,3))
-//  ОШИБКА
+function sumNumbers(n1: number, n2 = 0, n3 = 0, n4 = 0, n5 = 0) {
+    return n1 + n2 + n3 + n4 + n5
+}
+console.log(sumNumbers(8, 9, 7, 9, 8))
 
 // задание 8
 // Написать функцию, которая принимает от 1 до 5 чисел и
 // возвращает большее из них.
+
+
+
+//практика 5 РЕКУРСИИ
+// задание 1
+// Написать функцию, которая вычисляет факториал задан-
+// ного числа.
+function getFactorialOfNum(n: number): number {
+    if (n == 1) {
+        return n
+    } else {
+        return n * getFactorialOfNum(n - 1)
+    }
+}
+console.log(getFactorialOfNum(6))
+
+// задание 2
+// Написать функцию, которая выводит все числа из заданного
+// пользователем диапазона в прямом порядке. И еще одну
+// функцию – для вывода в обратном порядке.
+function showAllNum(start: number, end: number): void {
+    if (start > end) {
+        return
+    }
+    console.log(start)
+    showAllNum(start+1, end)
+}
+showAllNum(1, 5)
+
+
+function showAllNumBack(start: number, end: number): void {
+    if (start < end) {
+        return
+    }
+    console.log(start)
+    showAllNumBack(start-1, end)
+}
+showAllNumBack(5, 1)
+
+
+// задание 3
+// Написать функцию, которая выводит переданное ей число задом наперед.
+// Например: число 1234 вывести как 4321
+function getReverseNum(n: number): number {
+    if (n < 10) {
+        return n
+    } else {
+        return + (n % 10 + '' + getReverseNum(Math.floor(n / 10)))
+    }
+}
+console.log(getReverseNum(9849))
+
+// задание 4
+// Написать функцию, которая считает сумму цифр числа.
+// Например: число 1357, сумма 1 + 3 + 5 + 7 = 16
+function getSumOfDigits(n: number): number {
+    if (n < 10) {
+        return n
+    } else {
+        return n % 10 + getSumOfDigits(Math.floor(n / 10))
+    }
+}
+console.log(getSumOfDigits(344))
+
+// задание 5
+// Написать функцию, которая принимает число и выводит
+// соответствующее количество вложенных пар круглых скобок.
+// Например: число 4 – (((()))).
+function getBracketsPairsByNum(n: number): string {
+    if (!n) {
+        return ''
+    } else {
+        return '(' + getBracketsPairsByNum(n - 1) + ')'
+    }
+}
+
+// return '(' + getBracketsPairsByNum(4) +')' = return '(' + getBracketsPairsByNum(3) +')' =(((())))
+// return '(' + getBracketsPairsByNum(3) +')'= return '(' + getBracketsPairsByNum(2) +')' = ((()))
+// return '(' + getBracketsPairsByNum(2) +')' = return '(' + getBracketsPairsByNum(1) +')' =(())
+// return '(' + getBracketsPairsByNum(1) +')'= return '(' + getBracketsPairsByNum(0) +')' =()
+
+console.log(getBracketsPairsByNum(4)) 
