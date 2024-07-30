5e495fa84937f933a2bb1b8d1e974566a005bdb0
razumcevvaa <katerazumceva@gmail.com>
2024-07-30 13:45:32 +0300

    serverrrr

--- src/main.ts
+++ src/main.ts
@@ -3044,11 +3044,67 @@ random.addEventListener('click', () => {
 
 // поиск фильмов
 const titleFilm = document.getElementById("title-film") as HTMLInputElement
-const typeFilm= document.getElementById("type-film") as HTMLSelectElement
+const typeFilm = document.getElementById("type-film") as HTMLSelectElement
 const searchButton = document.getElementById("search-button") as HTMLButtonElement
-const resultsFilm = document.getElementById("result-film")  as HTMLDivElement
+const resultsFilm = document.getElementById("result-film") as HTMLDivElement
 
+searchButton.addEventListener('click', () => {
+    const title = titleFilm.value
+    const type = typeFilm.value
 
+    fetch(`http://www.omdbapi.com/?apikey=73558a7a&t=${title}&type=${type}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.Search && data.Search.length > 0) {
+                data.Search.forEach(movie => {
+                    // const movieElement = document.createElement("div")
+                    resultsFilm.innerHTML =
+                        `<img src="${movie.Poster}" alt = "${movie.Title}" >
+                            <h3>${movie.Title} </h3>
+                                <p> Type: ${movie.Type} </p>
+                                    <button onclick = ${showDetails(movie.imdbID)}> Details </button>`
+                    // resultsFilm.appendChild(movieElement)
+                })
+            } else {
+                const notFoundMessage = document.createElement("p")
+                notFoundMessage.innerText = "Movie not found!"
+                resultsFilm.appendChild(notFoundMessage)
+            }
+        })
+        .catch(error => {
+            console.log("An error occurred:", error)
+        })
+})
+
+function showDetails(imdbID) {
+    fetch(`http://www.omdbapi.com/?apikey=73558a7a&i=${imdbID}`)
+        .then(response => response.json())
+        .then(data => {
+            const movieDetails = document.createElement("div");
+            movieDetails.innerHTML =
+                `<h3>${data.Title}</h3>
+                <p>Released: ${data.Released}</p>
+                <p>Genre: ${data.Genre}</p>
+                <p>Director: ${data.Director}</p>
+                <p>Actors: ${data.Actors}</p>
+                <p>Plot: ${data.Plot}</p>`
+            resultsFilm.appendChild(movieDetails)
+        })
+        .catch(error => {
+            console.log("An error occurred:", error)
+        })
+}
+
+try {
+    axios('http://localhost:5173/JS-project/').then((resp) => console.log(resp.data))
+    fetch('http://localhost:5173/JS-project/')
+        .then((resp) => resp.json())
+        .then((data) => { console.log(data) })
+} catch (e) {
+    console.log('Не включен сервер')
+}
+let search = 'element'
+axios(`http://www.omdbapi.com/?apikey=73558a7a&s=${search}&page=2`).then((resp) => console.log(resp.data))
 
 
 
