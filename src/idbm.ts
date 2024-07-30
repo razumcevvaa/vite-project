d22302650e606218ab8bc4f859cb843128b93c18
razumcevvaa <katerazumceva@gmail.com>
2024-07-30 20:32:19 +0300

    page2

---
+++ src/idbm.ts
@@ -0,0 +1,1 @@
+import './idbm.scss'
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,15 @@
+import { resolve } from 'path'
+
 export default {
-    base:'/JS-project/'
+    base: '/JS-project/',
+    build: {
+        rollupOptions: {
+            input: {
+                // @ts-ignore
+                main: resolve(__dirname, 'index.html'),
+                // @ts-ignore
+                page2: resolve(__dirname, 'idbm.html'),
+            }
+        }
+    }
 }
